b12747591fa20145cb163585b255eb01f230ca77
nicolasODT <nicolas.terwagne@gmail.com>
2023-10-23 23:22:40 +0200

    Add Personne struct and Saluer trait implementation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,40 @@
+fn main() {
+    let nom = "Rust";
+    println!("{}", saluer(nom));
 
+    let personne = Personne::new("Rust".to_string(), 5, Etat::Actif);
+    personne.saluer();
+}
 
 fn saluer(nom: &str) -> String {
     format!("Bonjour, {}!", nom)
 }
 
-fn main() {
-    let resultat = saluer("Alice");
-    println!("{}", resultat); // Affiche "Bonjour, Alice!"
+struct Personne {
+    nom: String,
+    age: u32,
+    etat: Etat,
+}
+#[derive(Debug)]
+enum Etat {
+    Actif,
+}
+
+impl Personne {
+    fn new(nom: String, age: u32, etat: Etat) -> Self {
+        Personne { nom, age, etat }
+    }
+}
+
+trait Saluer {
+    fn saluer(&self);
+}
+
+impl Saluer for Personne {
+    fn saluer(&self) {
+        println!(
+            "Bonjour, je suis {} et j'ai {} ans. Mon état est {:?}",
+            self.nom, self.age, self.etat
+        );
+    }
 }
