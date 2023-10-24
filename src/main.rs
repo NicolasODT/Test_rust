8651f9780eda6a9b2ed2bd661d4e2d7455c1485f
nicolasODT <nicolas.terwagne@gmail.com>
2023-10-24 22:30:30 +0200

    Add comment

--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,37 @@ fn main() {
     personne.saluer();
 }
 
+// saluer est une fonction qui prend un paramètre nom de type &str et retourne une String / str qui est le résultat de la fonction format!.
 fn saluer(nom: &str) -> String {
     format!("Bonjour, {}!", nom)
 }
 
+// Personne est une structure qui a trois champs: nom, age et etat. Le champ etat est de type Etat qui est une énumération avec un seul membre Actif.
 struct Personne {
-    nom: String,
-    age: u32,
-    etat: Etat,
+    nom: String, // String est une chaîne de caractères allouée sur le tas.
+    age: u32, // u32 est un entier non signé sur 32 bits / non signe qui signifie qu'il ne peut pas être négatif.
+    etat: Etat, // Etat est une énumération.
 }
-#[derive(Debug)]
+
+// Etat est une énumération avec un seul membre Actif.
+#[derive(Debug)] // Debug est un trait qui permet d'afficher le contenu de l'énumération.
 enum Etat {
     Actif,
 }
-
+// impl est un mot-clé qui permet d'implémenter des méthodes pour une structure ou une énumération.
+// impl Personne permet d'implémenter des méthodes pour la structure Personne.
 impl Personne {
     fn new(nom: String, age: u32, etat: Etat) -> Self {
         Personne { nom, age, etat }
     }
 }
-
+// la difference entre impl et trait est que impl permet d'implémenter des méthodes pour une structure ou une énumération et trait permet de définir des méthodes pour une structure ou une énumération.
+// Saluer est un trait qui permet de définir des méthodes pour une structure ou une énumération.
 trait Saluer {
     fn saluer(&self);
 }
 
+// impl Saluer for Personne permet d'implémenter des méthodes pour la structure Personne.
 impl Saluer for Personne {
     fn saluer(&self) {
         println!(
